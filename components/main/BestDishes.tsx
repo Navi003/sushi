6987de5867a526959afab82a8904fa5970326dd3
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-01-19 21:47:18 +0100

    added bes dishes section

--- components/main/BestDishes.tsx
+++ components/main/BestDishes.tsx
@@ -2,13 +2,23 @@ import { FC } from "react";
 import React from "react";
 import HeadingSecondary from "../ui/HeadingSecondary";
 import styles from "./BesDishes.module.css";
+import DishCard from "../ui/DishCard";
 interface BestDishesProps {}
 const BestDishes: FC<BestDishesProps> = (): JSX.Element => {
   return (
-    <section>
+    <section
+      style={{
+        height: "100vh",
+      }}
+    >
       <div className={styles.headingBox}>
         <HeadingSecondary>Special Dishes</HeadingSecondary>
-        <h2 className={styles.headingTers}>Best Dishes From Our Menu</h2>
+        <h2 className={styles.headingText}>Best Dishes From Our Menu</h2>
+      </div>
+      <div className={styles.bestDishes}>
+        <DishCard />
+        <DishCard />
+        <DishCard />
       </div>
     </section>
   );
