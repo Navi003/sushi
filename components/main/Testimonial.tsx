e2e39e8d3287ab8461dbf07ff36a95a810e9f036
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-01-21 11:49:59 +0100

    added Testimonal Section

--- components/main/Testimonial.tsx
+++ components/main/Testimonial.tsx
@@ -7,8 +7,8 @@ interface TestimonialProps {}
 
 const Testimonial: FC<TestimonialProps> = (): JSX.Element => {
   return (
-    <div className={styles.testimonial}>
-      <div>
+    <section className={styles.testimonial}>
+      <div className={styles.imageBox}>
         <Image
           src="/sushi-cook.png"
           height={600}
@@ -16,13 +16,41 @@ const Testimonial: FC<TestimonialProps> = (): JSX.Element => {
           alt="sushi-cook"
         />
       </div>
-      <div className={styles.headingBox}>
-        <HeadingSecondary customStyles={{ textAlign: "left" }}>
-          Testimonial
-        </HeadingSecondary>
-        <h2 className={styles.headingTers}>What Our Customers Say About Us</h2>
+      <div>
+        <div className={styles.headingBox}>
+          <HeadingSecondary
+            className={styles.headingSec}
+            customStyles={{ textAlign: "left" }}
+          >
+            Testimonial
+          </HeadingSecondary>
+          <h2 className={styles.headingTers}>
+            What Our Customers Say About Us
+          </h2>
+        </div>
+        <div>
+          <p className={styles.text}>
+            “I had the pleasure of dining at Foodi last night, and I'm still
+            raving about the experience! The attention to detail in presentation
+            and service was impeccable”
+          </p>
+          <div className={styles.customerFeedbackContainer}>
+            <Image
+              width={200}
+              height={90}
+              src="/group.png"
+              alt="Group of People who Like our food"
+            />
+            <div className={styles.customerFeedback}>
+              <h6>Customer Feedback</h6>
+              <div>
+                <span>4.9</span> <span>(18.6k Reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 export default Testimonial;
--- components/ui/HeadingSecondary.tsx
+++ components/ui/HeadingSecondary.tsx
@@ -4,13 +4,18 @@ import styles from "./HeadingSecondary.module.css";
 interface HeadingSecondaryProps {
   children: React.ReactNode;
   customStyles?: {};
+  className?: String;
 }
 const HeadingSecondary: FC<HeadingSecondaryProps> = ({
   children,
   customStyles,
+  className,
 }): JSX.Element => {
   return (
-    <h3 style={customStyles} className={styles.headingSecondary}>
+    <h3
+      style={customStyles}
+      className={`${styles.headingSecondary} ${className ? className : ""}`}
+    >
       {children}
     </h3>
   );
